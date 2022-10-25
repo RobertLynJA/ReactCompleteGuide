2f21b51e2ce6894caac887b8dc653e4f6d934c9e
Robert Lyn <lenny4422@gmail.com>
2022-10-25 00:15:17 -0400

    Add event handler for new expense

--- section4/src/components/NewExpense/NewExpense.js
+++ section4/src/components/NewExpense/NewExpense.js
@@ -2,9 +2,21 @@ import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
-    return <div className="new-expense">
-        <ExpenseForm />
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+
+        props.onAddExpense(expenseData);
+    };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
+  );
 };
 
 export default NewExpense;
