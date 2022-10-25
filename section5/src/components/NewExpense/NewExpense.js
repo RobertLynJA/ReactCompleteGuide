3f753885cdd8725da9158f67cd70eebc3b8e3652
Robert Lyn <lenny4422@gmail.com>
2022-10-25 18:02:56 -0400

    Add show/hide new expense

--- section5/src/components/NewExpense/NewExpense.js
+++ section5/src/components/NewExpense/NewExpense.js
@@ -1,5 +1,6 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
 const NewExpense = (props) => {
 
@@ -12,9 +13,12 @@ const NewExpense = (props) => {
         props.onAddExpense(expenseData);
     };
 
+    let [isFormVisible, updateIsFormVisible] = useState(false);
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isFormVisible && <button onClick={() => updateIsFormVisible(true)}>Add New Expense</button>}
+      {isFormVisible && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={() => updateIsFormVisible(false)} />}
     </div>
   );
 };
