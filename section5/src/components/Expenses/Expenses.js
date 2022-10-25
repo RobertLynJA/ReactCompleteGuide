b29548a81a032a60351a9090823662e4ba5ad56f
Robert Lyn <lenny4422@gmail.com>
2022-10-25 16:33:18 -0400

    Add iteration for expenses

--- section5/src/components/Expenses/Expenses.js
+++ section5/src/components/Expenses/Expenses.js
@@ -5,7 +5,6 @@ import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 
 function Expenses(props) {
-
   const [filteredYear, setFilteredYear] = useState("2020");
 
   const onChangeFilterHandler = (selectedYear) => {
@@ -16,28 +15,18 @@ function Expenses(props) {
 
   return (
     <div>
-      <ExpensesFilter selected={filteredYear} onChangeFilter={onChangeFilterHandler} />
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={onChangeFilterHandler}
+      />
       <Card className="expenses">
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        ></ExpenseItem>
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
