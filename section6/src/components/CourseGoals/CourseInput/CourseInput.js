16bfeba759a01444b281d479d129355d52880e9f
Robert Lyn <lenny4422@gmail.com>
2022-10-26 18:00:13 -0400

    Use css styles for error state

--- section6/src/components/CourseGoals/CourseInput/CourseInput.js
+++ section6/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -9,6 +9,7 @@ const CourseInput = props => {
 
   const goalInputChangeHandler = event => {
     if (event.target.value.trim().length > 0) {
+      console.log("true");
       setIsValid(true);
     }
     setEnteredValue(event.target.value);
@@ -18,6 +19,7 @@ const CourseInput = props => {
     event.preventDefault();
 
     if (enteredValue.trim().length === 0) {
+      console.log("false");
       setIsValid(false);
       return;
     }
@@ -27,10 +29,9 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
-        <input style={{borderColor: !isValid ? 'red' : 'black',
-      background: !isValid ? 'salmon' : 'transparent'}} type="text" onChange={goalInputChangeHandler} />
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
