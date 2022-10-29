acddade57a5f560f55c440731aa17bfb5da56edc
Robert Lyn <lenny4422@gmail.com>
2022-10-28 22:55:44 -0400

    Style user input

--- section8/src/Components/Users/AddUser.js
+++ section8/src/Components/Users/AddUser.js
@@ -1,16 +1,21 @@
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
 const AddUser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (years)</label>
-      <input id="username" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (years)</label>
+        <input id="username" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
