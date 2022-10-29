eff937facc42d9b90e54d3c9d444f817ff1d7150
Robert Lyn <lenny4422@gmail.com>
2022-10-28 22:44:11 -0400

    Add AddUser component

--- section8/src/App.js
+++ section8/src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
-
+import AddUser from "./Components/Users/AddUser";
 
 function App() {
   return (
     <div>
-
+      <AddUser />
     </div>
   );
 }
---
+++ section8/src/Components/Users/AddUser.js
@@ -0,0 +1,17 @@
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text" />
+      <label htmlFor="age">Age (years)</label>
+      <input id="username" type="number" />
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+
+export default AddUser;
