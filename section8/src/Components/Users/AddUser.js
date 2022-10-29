9cacc4d412a98f3ac33ded87ef7380b72ab3dc61
Robert Lyn <lenny4422@gmail.com>
2022-10-28 22:59:28 -0400

    Style button

--- section8/src/Components/Users/AddUser.js
+++ section8/src/Components/Users/AddUser.js
@@ -1,5 +1,6 @@
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
+import Button from "../UI/Button";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -13,7 +14,7 @@ const AddUser = (props) => {
         <input id="username" type="text" />
         <label htmlFor="age">Age (years)</label>
         <input id="username" type="number" />
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
