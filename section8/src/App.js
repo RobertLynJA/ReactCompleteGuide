ac7dfd1685c0509ffea72313d5927fdd79c16d18
Robert Lyn <lenny4422@gmail.com>
2022-10-31 16:03:15 -0400

    Create user list

--- section8/src/App.js
+++ section8/src/App.js
@@ -1,9 +1,11 @@
 import AddUser from "./Components/Users/AddUser";
+import UsersList from "./Components/Users/UsersList";
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
