8da0ddca39a2d76efabc8bf777781b5df8091738
Robert Lyn <lenny4422@gmail.com>
2022-11-01 17:16:34 -0400

    Change effect to only use valid dependency

--- section10-2/src/components/Login/Login.js
+++ section10-2/src/components/Login/Login.js
@@ -49,11 +49,14 @@ const Login = (props) => {
     };
   }, []);
 
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid } = passwordState;
+
   useEffect(() => {
     const identifier = setTimeout(() => {
       console.log('Checking form validity!');
       setFormIsValid(
-        emailState.isValid && passwordState.isValid
+        emailIsValid && passwordIsValid
       );
     }, 500);
 
@@ -61,7 +64,7 @@ const Login = (props) => {
       console.log('CLEANUP');
       clearTimeout(identifier);
     };
-  }, [emailState, passwordState]);
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({
