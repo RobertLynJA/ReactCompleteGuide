bf21158ab93d856b431e9bd60aa2d7e5fbaa4f1f
Robert Lyn <lenny4422@gmail.com>
2022-11-20 19:49:23 -0500

    Move users into context

--- section13/src/components/UserFinder.js
+++ section13/src/components/UserFinder.js
@@ -2,14 +2,11 @@ import { Fragment, useState, useEffect, Component } from "react";
 
 import Users from "./Users";
 import classes from "./UserFinder.module.css";
-
-const DUMMY_USERS = [
-  { id: "u1", name: "Max" },
-  { id: "u2", name: "Manuel" },
-  { id: "u3", name: "Julie" },
-];
+import UsersContext from "../store/users-context";
 
 class UserFinder extends Component {
+  static contextType = UsersContext;
+
   constructor() {
     super();
 
@@ -22,13 +19,13 @@ class UserFinder extends Component {
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState(
-        {filteredUsers: DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm))}
+        {filteredUsers: this.context.users.filter((user) => user.name.includes(this.state.searchTerm))}
       );
     };
   }
 
   componentDidMount() {
-    this.setState({ filteredUsers: DUMMY_USERS });
+    this.setState({ filteredUsers: this.context.users });
   }
 
   searchChangeHandler(event) {
