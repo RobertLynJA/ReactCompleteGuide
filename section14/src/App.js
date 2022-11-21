ca904ca897accdfcd18327f73a57a09c1f47a915
Robert Lyn <lenny4422@gmail.com>
2022-11-21 18:20:44 -0500

    Change request to async/await

--- section14/src/App.js
+++ section14/src/App.js
@@ -1,28 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films/')
-      .then(response => response.json())
-      .then (data => {
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
 
-        const transformedMovies = data.results.map(movieData => { 
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            releaseDate: movieData.release_date,
-            openingText: movieData.opening_crawl
-          };
-        });
-        
-        setMovies(transformedMovies);
-      });
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        releaseDate: movieData.release_date,
+        openingText: movieData.opening_crawl,
+      };
+    });
+
+    setMovies(transformedMovies);
   }
 
   return (
