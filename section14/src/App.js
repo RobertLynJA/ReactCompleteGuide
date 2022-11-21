4aaefa138678d3be10a7e4c23e38506842d5772a
Robert Lyn <lenny4422@gmail.com>
2022-11-21 18:24:20 -0500

    Show loading and empty messages

--- section14/src/App.js
+++ section14/src/App.js
@@ -5,8 +5,10 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
@@ -20,6 +22,7 @@ function App() {
     });
 
     setMovies(transformedMovies);
+    setIsLoading(false);
   }
 
   return (
@@ -28,7 +31,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading Data!</p>}
+        {!isLoading && movies.length === 0 && <p>No movies found!</p>}
       </section>
     </React.Fragment>
   );
