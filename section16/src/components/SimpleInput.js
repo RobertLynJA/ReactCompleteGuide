e3f160726ef41ec8debfa44f04f5abd2ea71c3e7
Robert Lyn <lenny4422@gmail.com>
2022-11-29 21:56:46 -0500

    Get input value byRef and on change

--- section16/src/components/SimpleInput.js
+++ section16/src/components/SimpleInput.js
@@ -1,9 +1,25 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const nameInputRef = useRef();
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = event => {
+    event.preventDefault();
+
+    console.log(nameInputRef.current.value);
+    console.log(enteredName);
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input type="text" id="name" onChange={nameInputChangeHandler} ref={nameInputRef} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
