8de477703010ae766eaf7ec5e1774f95cc419bbc
Robert Lyn <lenny4422@gmail.com>
2022-11-29 22:46:01 -0500

    Add state to keep track of form

--- section16/src/components/SimpleInput.js
+++ section16/src/components/SimpleInput.js
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('');
   const nameInputRef = useRef();
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -12,19 +13,25 @@ const SimpleInput = (props) => {
     event.preventDefault();
 
     if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
       return;
     }
+
+    setEnteredNameIsValid(true);
 
     console.log(nameInputRef.current.value);
     console.log(enteredName);
   };
 
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input type="text" id="name" onChange={nameInputChangeHandler} ref={nameInputRef} />
       </div>
+      {!enteredNameIsValid && <p className="error-text">Name must not be empty</p>}
       <div className="form-actions">
         <button>Submit</button>
       </div>
