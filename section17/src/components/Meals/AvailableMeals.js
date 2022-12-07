4077c34cf1c0fd7cb2320705dab11dd385fca579
Robert Lyn <lenny4422@gmail.com>
2022-12-06 19:55:58 -0500

    Handle errors on fetch

--- section17/src/components/Meals/AvailableMeals.js
+++ section17/src/components/Meals/AvailableMeals.js
@@ -8,12 +8,18 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
         "https://prefab-rampart-351922-default-rtdb.firebaseio.com/meals.json"
       );
+      
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -31,8 +37,17 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
+
+  if (httpError) {
+    return <section className={classes.mealsError}>
+      <p>{httpError}</p>
+    </section>;
+  }
 
   if (isLoading) {
     return (
