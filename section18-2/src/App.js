df1c6d4bc4e1e8ecdbf6193264d98afbef15cee6
Robert Lyn <lenny4422@gmail.com>
2022-12-07 22:58:28 -0500

    Use Auth slice to handle login/logout

--- section18-2/src/App.js
+++ section18-2/src/App.js
@@ -1,11 +1,17 @@
 import Counter from './components/Counter';
 import Header from './components/Header'
 import Auth from './components/Auth'
+import UserProfile from './components/UserProfile';
+
+import { useSelector } from 'react-redux';
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
   return (<>
       <Header />
-      <Auth />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
       <Counter />
     </>
   );
