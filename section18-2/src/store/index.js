01743169d612cdb57602d863465575f3bfb3eb10
Robert Lyn <lenny4422@gmail.com>
2022-12-07 22:38:11 -0500

    Add redux slice for Auth

--- section18-2/src/store/index.js
+++ section18-2/src/store/index.js
@@ -1,10 +1,10 @@
 import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const initialState = { counter: 0, showCounter: true };
+const initialCounterState = { counter: 0, showCounter: true };
 
 const counterSlice = createSlice({
-  name: 'counter',
-  initialState,
+  name: "counter",
+  initialState: initialCounterState,
   reducers: {
     increment(state) {
       state.counter++;
@@ -17,13 +17,32 @@ const counterSlice = createSlice({
     },
     toggleCounter(state) {
       state.showCounter = !state.showCounter;
-    }
-  }
+    },
+  },
 });
 
-const store = configureStore({ 
-  reducer: counterSlice.reducer
+const initialAuthState = {
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+const store = configureStore({
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
 export default store;
