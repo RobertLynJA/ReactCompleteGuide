9b0aab04789d712e3d971ef84e6a74535bc8be2c
Robert Lyn <lenny4422@gmail.com>
2022-12-12 16:28:44 -0500

    Update server cart using useEffect

--- section19/src/App.js
+++ section19/src/App.js
@@ -1,10 +1,19 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://test-1f8fc-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
