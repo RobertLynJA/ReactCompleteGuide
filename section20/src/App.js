f998991cbe1dc4cf9d3ae79e27daa2fabce68d73
Robert Lyn <lenny4422@gmail.com>
2022-12-20 22:34:48 -0500

    Handling multiple matches with Switch and exact

--- section20/src/App.js
+++ section20/src/App.js
@@ -15,11 +15,11 @@ function App() {
           <Route path="/welcome">
             <Welcome />
           </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
           <Route path="/products/:productId">
             <ProductDetail />
-          </Route>
-          <Route path="/products">
-            <Products />
           </Route>
         </Switch>
       </main>
