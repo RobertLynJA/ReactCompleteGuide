9fd205fc574669d27206b91ebcae377e879dc528
Robert Lyn <lenny4422@gmail.com>
2022-12-20 22:10:34 -0500

    Use Link to navigate to routes

--- section20/src/App.js
+++ section20/src/App.js
@@ -1,16 +1,22 @@
-import { Route } from 'react-router-dom'
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
+import { Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <header>
+        <MainHeader />
+      </header>
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Products />
+        </Route>
+      </main>
     </div>
   );
 }
