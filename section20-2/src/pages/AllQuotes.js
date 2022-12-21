4f354919b385dfc6e758c0afde15a0b0b7169f58
Robert Lyn <lenny4422@gmail.com>
2022-12-20 23:18:52 -0500

    Add dummy quote data, add new quote form

--- section20-2/src/pages/AllQuotes.js
+++ section20-2/src/pages/AllQuotes.js
@@ -1,7 +1,12 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Tom', text: 'Learning React is great!' }
+];
+
 const AllQuote = () => {
-  return <h1>
-    All Quotes Page
-  </h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />
 };
 
 export default AllQuote;
--- section20-2/src/pages/NewQuote.js
+++ section20-2/src/pages/NewQuote.js
@@ -1,7 +1,11 @@
+import QuoteForm from "../components/quotes/QuoteForm";
+
 const NewQuote = () => {
-  return <h1>
-    New Quote
-  </h1>;  
+  const addQuoteHandler = quoteData => {
+    console.log(quoteData);
+  };
+
+  return <QuoteForm onAddQuote={addQuoteHandler} />; 
 };
 
 export default NewQuote;
