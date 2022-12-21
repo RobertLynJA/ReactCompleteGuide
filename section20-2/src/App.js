05e3c87cade2a55a7e12179d7235aea332de1cf9
Robert Lyn <lenny4422@gmail.com>
2022-12-20 23:13:51 -0500

    Add site layout and navigation

--- section20-2/src/App.js
+++ section20-2/src/App.js
@@ -1,24 +1,27 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
-import AllQuote from './pages/AllQuotes';
-import NewQuote from './pages/NewQuote';
-import QuoteDetail from './pages/QuoteDetail';
+import { Route, Switch, Redirect } from "react-router-dom";
+import AllQuote from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes" />
-      </Route>
-      <Route path="/quotes" exact>
-        <AllQuote />
-      </Route>
-      <Route path="/quotes/:quoteId">
-        <QuoteDetail />
-      </Route>
-      <Route path="/new-quote">
-        <NewQuote />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/quotes" />
+        </Route>
+        <Route path="/quotes" exact>
+          <AllQuote />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
