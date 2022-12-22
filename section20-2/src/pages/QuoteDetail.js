4f3ad921618b9e07ec268b185d59746798d2808d
Robert Lyn <lenny4422@gmail.com>
2022-12-21 22:54:25 -0500

    Get data from firebase

--- section20-2/src/pages/QuoteDetail.js
+++ section20-2/src/pages/QuoteDetail.js
@@ -1,26 +1,43 @@
 import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+import { useEffect } from 'react';
 
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Max", text: "Learning React is fun!" },
-  { id: "q2", author: "Tom", text: "Learning React is great!" },
-];
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
 const QuoteDetail = () => {
   const match = useRouteMatch();
   const params = useParams();
 
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const { quoteId } = params;
 
-  if (!quote) {
-    return <p>No Quote found!</p>;
+  const { sendRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);    
+  }, [sendRequest, quoteId]);
+
+  if (status === 'pending') {
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    );
   }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+    if (!loadedQuote.text) {
+      return <p>No Quote found!</p>;
+    }
 
   return (
     <>
-      <HighlightedQuote text={quote.text} author={quote.author} />
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
       <Route path={`${match.path}`} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>
