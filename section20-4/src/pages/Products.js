6ad3a8b873ac5b3731bfc7b7a72b7354916e3700
Robert Lyn <lenny4422@gmail.com>
2022-12-22 00:12:16 -0500

    Some comments on ways to use navigate

--- section20-4/src/pages/Products.js
+++ section20-4/src/pages/Products.js
@@ -1,6 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Products = () => {
+  const navigate = useNavigate();
+  //navigate('/welcome'); //Navigate programatically
+  //navigate('/welcome', { replace: true }); //Redirect programatically
+  //navigate(1); // Go forward
+  //navigate(-1); // Go backward
+
   return (
     <section>
       <h1>The Products Page</h1>
