f4722c0c6cee527e9334e507612e1ffe3eba7bd9
Robert Lyn <lenny4422@gmail.com>
2022-12-23 19:02:55 -0500

    Use react router to submit a form

--- section20-5/src/App.jsx
+++ section20-5/src/App.jsx
@@ -8,7 +8,7 @@ import {
 
 import BlogLayout from "./pages/BlogLayout";
 import BlogPostsPage, { loader as blogPostsLoader } from "./pages/BlogPosts";
-import NewPostPage from "./pages/NewPost";
+import NewPostPage, { action as newPostAction } from "./pages/NewPost";
 import PostDetailPage, { loader as blogPostLoader } from "./pages/PostDetail";
 import RootLayout from "./pages/RootLayout";
 import WelcomePage from "./pages/Welcome";
@@ -26,7 +26,7 @@ const router = createBrowserRouter(
           loader={blogPostLoader}
         />
       </Route>
-      <Route path="/blog/new" element={<NewPostPage />} />
+      <Route path="/blog/new" element={<NewPostPage />} action={newPostAction} />
     </Route>
   )
 );
--- section20-5/src/pages/NewPost.jsx
+++ section20-5/src/pages/NewPost.jsx
@@ -1,45 +1,42 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { redirect, useNavigate, useActionData } from "react-router-dom";
 
-import NewPostForm from '../components/NewPostForm';
-import { savePost } from '../util/api';
+import NewPostForm from "../components/NewPostForm";
+import { savePost } from "../util/api";
 
 function NewPostPage() {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [error, setError] = useState();
+  const data = useActionData();
   const navigate = useNavigate();
 
-  async function submitHandler(event) {
-    event.preventDefault();
-    setIsSubmitting(true);
-    try {
-      const formData = new FormData(event.target);
-      const post = {
-        title: formData.get('title'),
-        body: formData.get('post-text'),
-      };
-      await savePost(post);
-      navigate('/');
-    } catch (err) {
-      setError(err);
-    }
-    setIsSubmitting(false);
-  }
-
   function cancelHandler() {
-    navigate('/blog');
+    navigate("/blog");
   }
 
   return (
     <>
-      {error && <p>{error.message}</p>}
-      <NewPostForm
-        onCancel={cancelHandler}
-        onSubmit={submitHandler}
-        submitting={isSubmitting}
-      />
+      { data && data.status && <p>{data.message}</p> }
+      <NewPostForm onCancel={cancelHandler} submitting={isSubmitting} />
     </>
   );
 }
 
 export default NewPostPage;
+
+export async function action({ request }) {
+  const formData = await request.formData();
+
+  const post = {
+    title: formData.get("title"),
+    body: formData.get("post-text"),
+  };
+
+  try {
+    await savePost(post);
+  } catch (error) {
+    if (error.status === 422) {
+      return error; //stay on page using return
+    }
+    throw error;
+  }
+  return redirect('/blog');
+}
