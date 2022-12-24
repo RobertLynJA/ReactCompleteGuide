4fcdefec447d9c959a29a3562a44d3ba577923eb
Robert Lyn <lenny4422@gmail.com>
2022-12-23 21:38:58 -0500

    Add another strongly typed component

--- section27/src/components/Todos.tsx
+++ section27/src/components/Todos.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import Todo from '../models/todo';
+import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
 
-const Todos: React.FC<{items: Todo[]}> = props => {
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
-        {props.items.map(item => <li key={item.id}>{item.text}</li>)}
+      {props.items.map((item) => (
+        <TodoItem key={item.id} text={item.text} />
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
