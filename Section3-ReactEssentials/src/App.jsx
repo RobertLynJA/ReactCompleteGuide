430b7757e0855541fbb46116f0cd73f977e19f74
Robert Lyn <43123823+RobertLynJA@users.noreply.github.com>
2025-09-25 23:28:46 -0400

    s3 add coreconcept component with props

--- Section3-ReactEssentials/src/App.jsx
+++ Section3-ReactEssentials/src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -21,11 +22,33 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title="Components" 
+              description="The core UI building block." 
+              image={componentsImg} />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
