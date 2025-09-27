14092107745fbabb192a500f18bee50e5faded40
Robert Lyn <43123823+RobertLynJA@users.noreply.github.com>
2025-09-27 15:04:55 -0400

    s3 add props for all data

--- Section3-ReactEssentials/src/App.jsx
+++ Section3-ReactEssentials/src/App.jsx
@@ -1,5 +1,6 @@
 import reactImg from './assets/react-core-concepts.png';
 import componentsImg from './assets/components.png';
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -41,12 +42,21 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept 
-              title="Components" 
-              description="The core UI building block." 
-              image={componentsImg} />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept />
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image} />
+            <CoreConcept 
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image} />
+            <CoreConcept 
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image} />
+            <CoreConcept 
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image} />
           </ul>
         </section>
         <h2>Time to get started!</h2>
