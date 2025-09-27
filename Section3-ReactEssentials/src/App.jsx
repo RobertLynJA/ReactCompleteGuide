1b9bc8cca817d19d15c02d8d3fcd11d8254564b4
Robert Lyn <43123823+RobertLynJA@users.noreply.github.com>
2025-09-27 16:25:16 -0400

    s3 move coreConcept to new file

--- Section3-ReactEssentials/src/App.jsx
+++ Section3-ReactEssentials/src/App.jsx
@@ -1,16 +1,7 @@
 import componentsImg from './assets/components.png';
 import { CORE_CONCEPTS } from './data.js';
 import Header from './Components/Header.jsx';
-
-function CoreConcept({image, title, description}) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import CoreConcept from './Components/CoreConcept.jsx';
 
 function App() {
   return (
