1c593d56927fbf9340419c16f8144bd7d13dbb62
Robert Lyn <43123823+RobertLynJA@users.noreply.github.com>
2025-09-27 21:13:15 -0400

    s3 use map() to render lists

--- Section3-ReactEssentials/src/App.jsx
+++ Section3-ReactEssentials/src/App.jsx
@@ -37,10 +37,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map(coreItem => (
+              <CoreConcept key={coreItem.title} {...coreItem} />
+            ))}
           </ul>
         </section>
         <section id="examples">
