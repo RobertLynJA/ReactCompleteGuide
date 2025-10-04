ebf2a8b24850179c770ad6d1659d4b54971a7441
robertlynja <lenny4422@gmail.com>
2025-10-04 17:48:20 -0400

    s4 create player component

--- Section4-ReactEssentials2-Part2/src/App.jsx
+++ Section4-ReactEssentials2-Part2/src/App.jsx
@@ -1,25 +1,12 @@
+import Player from './components/Player'
 
 function App() {
-  
-
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player">
-              <span className="player-name">Player 1</span>
-              <span className="player-symbol">X</span>
-            </span>
-            <button>Edit</button>
-          </li>
-          <li>
-            <span className="player">
-              <span className="player-name">Player 2</span>
-              <span className="player-symbol">O</span>
-            </span>
-            <button>Edit</button>
-          </li>
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
         </ol>
 
         GAME BOARD
